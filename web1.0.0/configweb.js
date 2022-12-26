fc9b179cdd72b0e934e4eec1e7b624b391490e67
Wichy Ph.  (PhD.) <30399464+semicon@users.noreply.github.com>
2022-12-26 22:20:05 +0700

    Update configweb.js

--- web1.0.0/configweb.js
+++ web1.0.0/configweb.js
@@ -6,10 +6,10 @@
  *       FB: https://facebook.com/construct2n3
  *       FB: https://facebook.com/ph.wichian
 */
-
+         
 function setconfig(){
       const dd = new Date()
-      document.querySelector('.copyleft').innerHTML = "Copyleft  <img src='https://mirrors.creativecommons.org/presskit/icons/heart.red.png' width='25' height='25' alt='cc'> 2010 - "+dd.getYear()+", by Dr.Wichian Phumphuang"
+      document.querySelector('.copyleft').innerHTML = "Copyleft  <img src='https://mirrors.creativecommons.org/presskit/icons/heart.red.png' width='25' height='25' alt='cc'> 2010 - "+document.write(dd.getYear())+", by Dr.Wichian Phumphuang"
       option_classroom()
       option_workshop()
     }
