8897aee7352948098e45ac55158be8b83585aa95
Wichy Ph.  (PhD.) <30399464+semicon@users.noreply.github.com>
2022-12-26 20:33:56 +0700

    Create configweb.js

---
+++ configweb.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Dr.Wichian Pumphuang 
+ *     Blog: https://guruchian.blogspot.com
+ *   Github: https://github.com/semicon
+ *     Line: semisailom
+ *       FB: https://facebook.com/construct2n3
+ *       FB: https://facebook.com/ph.wichian
+*/
+
+function setconfig(){
+      document.querySelector('.copyleft').innerHTML = "Copyleft  <img src='https://mirrors.creativecommons.org/presskit/icons/heart.red.png' width='25' height='25' alt='cc' \/> 2010 - <script>document.write(new Date().getFullYear())<\/script>, by Dr.Wichian Phumphuang"
+      option_classroom()
+      option_workshop()
+    }
