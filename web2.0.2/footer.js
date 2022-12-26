7b41f95a5d7c54f4b199727b0ac2578ebcc34aae
Wichy Ph.  (PhD.) <30399464+semicon@users.noreply.github.com>
2022-12-27 00:46:36 +0700

    Create footer.js

---
+++ web2.0.2/footer.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Dr.Wichian Pumphuang 
+ *     Blog: https://guruchian.blogspot.com
+ *   Github: https://github.com/semicon
+ *     Line: semisailom
+ *       FB: https://facebook.com/construct2n3
+ *       FB: https://facebook.com/ph.wichian
+*/
+         
+function setconfig(){
+      const dd = new Date()
+      document.querySelector('.copyleft').innerHTML = "Copyleft  <img src='https://mirrors.creativecommons.org/presskit/icons/heart.red.png' width='25' height='25' alt='cc'> @KruChian. Created by <a href='https://guruchian.blogspot.com'>Wichian Ph.</a>"
+    }
