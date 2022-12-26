f88a001d9c10834d128710249f69254cc94f6774
Wichy Ph.  (PhD.) <30399464+semicon@users.noreply.github.com>
2022-12-26 23:05:32 +0700

    Create webconfig.js

---
+++ web1.0.1/webconfig.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Dr.Wichian Pumphuang 
+ *     Blog: https://guruchian.blogspot.com
+ *   Github: https://github.com/semicon
+ *     Line: semisailom
+ *       FB: https://facebook.com/construct2n3
+ *       FB: https://facebook.com/ph.wichian
+*/
+         
+function setconfig(){
+      const dd = new Date()
+      document.querySelector('.copyleft').innerHTML = "Copyleft  <img src='https://mirrors.creativecommons.org/presskit/icons/heart.red.png' width='25' height='25' alt='cc'> 2010 - "+document.write(dd.getYear())+", by Dr.Wichian Phumphuang"
+    }
