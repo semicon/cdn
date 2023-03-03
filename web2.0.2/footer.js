f91b7f3f4a6e7724cb7fea9bb1f6206a19c086bf
Wichy Ph.  (PhD.) <30399464+semicon@users.noreply.github.com>
2023-03-03 21:19:43 +0700

    Update footer.js

--- web2.0.2/footer.js
+++ web2.0.2/footer.js
@@ -10,4 +10,5 @@
 function setconfig(){
       const dd = new Date()
       document.querySelector('.copyleft').innerHTML = "Copyleft  <img src='https://mirrors.creativecommons.org/presskit/icons/heart.red.png' width='25' height='25' alt='cc'> @KruChian. Created by <a href='https://guruchian.blogspot.com' target='_blank'>Wichian Ph.</a>"
+      document.querySelector('.template').innerHTML = "Copyleft  <img src='https://mirrors.creativecommons.org/presskit/icons/heart.red.png' width='25' height='25' alt='cc'> @KruChian. Created by <a href='https://guruchian.blogspot.com' target='_blank'>Wichian Ph.</a>"
     }
