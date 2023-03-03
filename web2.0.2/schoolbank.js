c526b1ba2014f9365b17b46c4feb310a7f546de5
Wichy Ph.  (PhD.) <30399464+semicon@users.noreply.github.com>
2023-03-03 21:53:00 +0700

    Update schoolbank.js

--- web2.0.2/schoolbank.js
+++ web2.0.2/schoolbank.js
@@ -9,5 +9,10 @@
          
 function setconfig(){
       document.querySelector('.copyleft').innerHTML = "Copyleft  <img src='https://mirrors.creativecommons.org/presskit/icons/heart.red.png' width='25' height='25' alt='cc'> @KruChian. Created by <a href='https://guruchian.blogspot.com' target='_blank'>Wichian Ph.</a>"
+      //document.querySelector('.template').innerHTML = "Copyleft  <img src='https://mirrors.creativecommons.org/presskit/icons/heart.red.png' width='25' height='25' alt='cc'> @KruChian. Created by <a href='https://guruchian.blogspot.com' target='_blank'>Wichian Ph.</a>"
+    }
+
+function settemplete(){
+      //document.querySelector('.copyleft').innerHTML = "Copyleft  <img src='https://mirrors.creativecommons.org/presskit/icons/heart.red.png' width='25' height='25' alt='cc'> @KruChian. Created by <a href='https://guruchian.blogspot.com' target='_blank'>Wichian Ph.</a>"
       document.querySelector('.template').innerHTML = "Copyleft  <img src='https://mirrors.creativecommons.org/presskit/icons/heart.red.png' width='25' height='25' alt='cc'> @KruChian. Created by <a href='https://guruchian.blogspot.com' target='_blank'>Wichian Ph.</a>"
     }
