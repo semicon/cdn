1689cc3e5b25f9ff726e780fb170f883b223a985
Wichy Ph.  (PhD.) <30399464+semicon@users.noreply.github.com>
2023-08-19 14:27:34 +0700

    Update parts.js

--- web3.0/webapps/libs/js/parts.js
+++ web3.0/webapps/libs/js/parts.js
@@ -1,11 +1,11 @@
 const datenow = new Date().getFullYear() ;
 const elemx = document.querySelector("body footer");
 const entries = elemx.innerHTML.match("Dr. Wichian ph.");
-let dd = ''
-		 if(datenow != '2023') {
-				dd = ' - '+datenow ;
-			}
-		if (!entries) {
-		    elemx.remove();
-		    document.write(`<footer>Copyright   <img src="https://mirrors.creativecommons.org/presskit/icons/heart.red.png" width="25" height="25" alt="cc" /> 2023${dd}, <a class="text-warning" href="https://guruchian.blogspot.com/" target=_blank> Dr. Wichian Ph.</a>   All Rights Reserved.</footer>`);
-		} 
+let dd = '';
+    if(datenow != '2023') {
+	dd = ' - '+datenow ;
+    }
+    if (!entries) {
+	elemx.remove();
+        document.write(`<footer>Copyright   <img src="https://mirrors.creativecommons.org/presskit/icons/heart.red.png" width="25" height="25" alt="cc" /> 2023${dd}, <a class="text-warning" href="https://guruchian.blogspot.com/" target=_blank> Dr. Wichian Ph.</a>   All Rights Reserved.</footer>`);
+    } 
