ef0e2cfd3eec4956ca14f9d2a7011234aa0fcdde
Wichy Ph.  (PhD.) <30399464+semicon@users.noreply.github.com>
2023-08-22 09:24:15 +0700

    Update parts.js

--- web3.0/webapps/libs/js/bootstrap-parts@0.1.1/parts.js
+++ web3.0/webapps/libs/js/bootstrap-parts@0.1.1/parts.js
@@ -1,32 +1,34 @@
-function showTime(){
-    const date = new Date();
-    const h = date.getHours(); // 0 - 23
-    const m = date.getMinutes(); // 0 - 59
-    const s = date.getSeconds(); // 0 - 59
-    let session = "AM";
-    
-    if(h == 0){
-        h = 12;
-    }
-    
-    if(h > 12){
-        h = h - 12;
-        session = "PM";
-    }
-    
-    h = (h < 10) ? "0" + h : h;
-    m = (m < 10) ? "0" + m : m;
-    s = (s < 10) ? "0" + s : s;
-    
-    let time = h + ":" + m + ":" + s + " " + session;
-    document.getElementById("MyClockDisplay").innerText = time;
-    document.getElementById("MyClockDisplay").textContent = time;
-    
-    setTimeout(showTime, 1000);
-    
+function showTime() {
+  var date = new Date();
+  var h = date.getHours(); // 0 - 23
+  var m = date.getMinutes(); // 0 - 59
+  var s = date.getSeconds(); // 0 - 59
+  var session = "AM";
+
+  if (h == 0) {
+    h = 12;
+  }
+
+  if (h > 12) {
+    h = h - 12;
+    session = "PM";
+  }
+
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
+  var time = h + ":" + m + ":" + s + " " + session;
+  document.getElementById("MyClockDisplay").innerText = time;
+  document.getElementById("MyClockDisplay").textContent = time;
+
+  setTimeout(showTime, 1000);
+
 }
 
 showTime();
+
+
 
 const  tags = document.getElementsByTagName("footer");
 if (tags.length == 0) {
