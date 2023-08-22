1fe5e7d35802b441d088f39906e513aff1011b85
Wichy Ph.  (PhD.) <30399464+semicon@users.noreply.github.com>
2023-08-22 10:23:17 +0700

    Update parts.js

--- web3.0/webapps/libs/js/bootstrap-parts@0.1.1/parts.js
+++ web3.0/webapps/libs/js/bootstrap-parts@0.1.1/parts.js
@@ -3,16 +3,16 @@ function showTime() {
   var h = date.getHours(); // 0 - 23
   var m = date.getMinutes(); // 0 - 59
   var s = date.getSeconds(); // 0 - 59
-  var session = "AM";
+  var session = "น.";
 
-  if (h == 0) {
-    h = 12;
+  if (h == 24) {
+    h = 0;
   }
 
-  if (h > 12) {
-    h = h - 12;
-    session = "PM";
-  }
+  //if (h > 12) {
+  //  h = h - 12;
+  //  session = "PM";
+  //}
 
   h = h < 10 ? "0" + h : h;
   m = m < 10 ? "0" + m : m;
