40eb120dcc147ff8f09f8e285d906a5466829e6f
Wichy Ph.  (PhD.) <30399464+semicon@users.noreply.github.com>
2023-10-08 23:05:01 +0700

    Create partfab.js

---
+++ web3.0/webapps/libs/js/bootstrap-parts@0.1.1/partfab.js
@@ -0,0 +1,26 @@
+$("#fab-active").html(`<div class="fab-container">
+      <div class="fab fab-icon-holder">
+        <i><img src="https://sv1.picz.in.th/images/2023/07/24/MolhWv.png" class="rounded-circle me-2" style="width: 75px;"></i>
+      </div>
+  
+      <ul class="fab-options">
+        <li>
+          <span class="fab-label">ติดตามเพจ Facebook</span>
+          <div class="fab-icon-holder">
+            <a target="_blank" href="https://www.facebook.com/construct2n3"><i class="fa-brands fa-facebook"></i></i></a>
+          </div>
+        </li>
+        <li>
+          <span class="fab-label">Email</span>
+          <div class="fab-icon-holder">
+            <a target="_blank" href="https://mail.google.com/mail/u/0/?fs=1&to=ph.wichian@gmail.com&tf=cm"><i class="fas fa-envelope"></i></a>
+          </div>
+        </li>
+        <li style="margin-bottom: 60px;">
+          <span class="fab-label">สนับสนุนโปรเจกต์</span>
+          <div class="fab-icon-holder">
+            <a target="_blank" href="https://guruchian.blogspot.com/p/donate.html"><i class="fa-solid fa-cart-shopping"></i></a>
+          </div>
+        </li>
+      </ul>
+</div>`)
