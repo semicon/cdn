34510260d1d80d217c933818b033b38e9826e478
Wichy Ph.  (PhD.) <30399464+semicon@users.noreply.github.com>
2023-10-09 09:37:51 +0700

    Update partfab.js

--- web3.0/webapps/libs/js/bootstrap-parts@0.1.1/partfab.js
+++ web3.0/webapps/libs/js/bootstrap-parts@0.1.1/partfab.js
@@ -1,23 +1,23 @@
 $("#fab-active").html(`<div class="fab-container">
-      <div class="fab fab-icon-holder">
-        <i><img src="https://github.com/semicon/semicon.github.io/blob/master/img/logosm.png" class="rounded-circle me-2" style="width: 75px;"></i>
+      <div class="fab">
+        <i><img src="https://semicon.github.io/img/logosm.png" class="rounded-circle" style="width: 60px;"></i>
       </div>
   
       <ul class="fab-options">
         <li>
-          <span class="fab-label">ติดตามเพจ Facebook</span>
+          <span class="fab-label">ติดตามเรา</span>
           <div class="fab-icon-holder">
-            <a target="_blank" href="https://web.facebook.com/construct2n3"><i class="fa-brands fa-facebook"></i></i></a>
+            <a target="_blank" href="https://web.facebook.com/construct2n3"><i class="fa-brands fa-facebook fs-1 text-primary"></i></i></a>
           </div>
         </li>
         <li>
-          <span class="fab-label">Email</span>
+          <span class="fab-label">ติดต่อเรา</span>
           <div class="fab-icon-holder">
-            <a target="_blank" href="https://mail.google.com/mail/u/0/?fs=1&to=ph.wichian@gmail.com&tf=cm"><i class="fas fa-envelope"></i></a>
+            <a target="_blank" href="https://mail.google.com/mail/u/0/?fs=1&to=ph.wichian@gmail.com&tf=cm"><i class="fas fa-envelope fs-2 text-danger"></i></a>
           </div>
         </li>
         <li style="margin-bottom: 60px;">
-          <span class="fab-label">สนับสนุนโปรเจกต์</span>
+          <span class="fab-label">สนับสนุนเรา</span>
           <div class="fab-icon-holder">
             <a target="_blank" href="https://guruchian.blogspot.com/p/donate.html"><img src="https://sv1.picz.in.th/images/2023/07/24/MolhWv.png" class="rounded-circle" style="width: 100%;"></a>
           </div>
