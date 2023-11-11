66842a436cecd4a96b666061704ebb2dfaa6a7ee
Wichy Ph.  (PhD.) <30399464+semicon@users.noreply.github.com>
2023-11-11 22:34:34 +0700

    Update partfab.js

--- web3.0/webapps/libs/js/bootstrap-parts@0.1.1/partfab.js
+++ web3.0/webapps/libs/js/bootstrap-parts@0.1.1/partfab.js
@@ -44,13 +44,13 @@ $("#fab-active").html(`<div class="fab-container">
         <li>
           <span class="fab-label">ติดตามเรา</span>
           <div class="fab-icon-holder">
-            <a target="_blank" href="https://web.facebook.com/construct2n3"><i class="fa-brands fa-facebook fs-1 text-primary"></i></i></a>
+            <a target="_blank" href="https://web.facebook.com/construct2n3"><i class="fa-brands fa-facebook text-primary"></i></i></a>
           </div>
         </li>
         <li>
           <span class="fab-label">ติดต่อเรา</span>
           <div class="fab-icon-holder">
-            <a target="_blank" href="https://mail.google.com/mail/u/0/?fs=1&to=ph.wichian@gmail.com&tf=cm"><i class="fas fa-envelope fs-1 text-danger"></i></a>
+            <a target="_blank" href="https://mail.google.com/mail/u/0/?fs=1&to=ph.wichian@gmail.com&tf=cm"><i class="fas fa-envelope text-danger"></i></a>
           </div>
         </li>
         <li style="margin-bottom: 50px;">
